5b620bca5418a82ff513731f5484fd6c46be7def
Ivan Vasilevich <ivanvbesarab@gmail.com>
2017-05-10 20:26:06 +0300

    read write from file

--- G54L5/ViewController.swift
+++ G54L5/ViewController.swift
@@ -13,11 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let numbers = ([1, 4, 9] as NSArray)
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString
+        let path = documentsPath.appendingPathComponent("Ваш квиток.plist")
+        let result = numbers.write(toFile: path, atomically: true)
+        print(path)
+        if result {
+            
+        }
+        else {
+            
+        }
+        
+        let arr = NSArray(contentsOfFile: path)
+        print(arr)
+        if let numbersArr = arr as? [Int] {
+            print(numbersArr)
+        }
+        
     }
 
 
